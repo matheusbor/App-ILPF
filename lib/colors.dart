770b5f1f199e7a87cdc7d40cf7d5ce3652366c69
matheusbor <matheuslb2003@gmail.com>
2024-10-04 15:59:23 -0300

    feat: added register screen

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static Color greenNormal = Color.fromRGBO(50, 205, 50, 1);
+  static Color greenLight = Color.fromRGBO(235, 250, 235, 1);
+  static Color greenLightActive = Color.fromRGBO(191, 240, 191, 1);
+}
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -10,41 +10,41 @@ class MyScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: RegisterScreen(),
     );
   }
-  
+
 }
 
-class LoginScreen extends StatelessWidget{
+class RegisterScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-         Center(child: Image.asset("app_assets/logo_menor.png" )),
+          Center(child: Image.asset("app_assets/logo_menor.png" )),
           SizedBox(height: 24,),
           Container(
             alignment: Alignment.topLeft,
             margin: EdgeInsets.only(left: 24, right: 24),
-            child: Text( "Fazer Login",
-              style: Theme.of(context).textTheme.displayMedium),
+            child: Text( "Criar conta",
+                style: Theme.of(context).textTheme.displayMedium),
           ),
           SizedBox(height: 16,),
           Container(
             alignment: Alignment.topLeft,
             margin: EdgeInsets.only(left: 24, right: 24),
             child: Text(
-                "Para criar e editar seus projetos, entre em sua conta ILPF.",
-            style: Theme.of(context).textTheme.bodyLarge,),
+              "Para criar e editar seus projetos, crie sua conta ILPF.",
+              style: Theme.of(context).textTheme.bodyLarge,),
           ),
           SizedBox(height: 24,),
           Container(
             margin: EdgeInsets.only(left: 24, right: 24, bottom: 8),
             child: TextField(
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Digite seu E-mail"
+                  border: OutlineInputBorder(),
+                  hintText: "Digite seu Nome"
               ),
             ),
           ),
@@ -53,13 +53,33 @@ class LoginScreen extends StatelessWidget{
             child: TextField(
               decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  hintText: "Digite sua Senha",
-                helperText: "Esqueçeu sua senha?",
-                helperStyle: Theme.of(context).textTheme.bodySmall,
+                  hintText: "Digite seu E-mail"
               ),
             ),
           ),
-          SizedBox(height: 4,),
+          Container(
+            margin: EdgeInsets.only(left: 24, right: 24, bottom: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Digite sua Senha",
+                //helperText: "Esqueçeu sua senha?",
+                //helperStyle: Theme.of(context).textTheme.bodySmall,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 24, right: 24, bottom: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Confirme sua Senha",
+                //helperText: "Esqueçeu sua senha?",
+                //helperStyle: Theme.of(context).textTheme.bodySmall,
+              ),
+            ),
+          ),
+          SizedBox(height: 16,),
           Container(
             margin: EdgeInsets.only(left: 24, right: 24, bottom: 4),
             alignment: Alignment.topRight,
@@ -68,10 +88,10 @@ class LoginScreen extends StatelessWidget{
                 Expanded(child: SizedBox()),
                 Expanded(
                   child: FilledButton(
-                  
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(MyColors.greenNormal),
-                    ),
+
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(MyColors.greenNormal),
+                      ),
                       onPressed: null,
                       child: Text("Avançar",style: TextStyle(color: Colors.white))),
                 ),
